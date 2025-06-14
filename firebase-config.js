0fcca64177890e1aa8f17e031fb550fb2405834b
sayyedmurtuza6456 <sayyedaliop942113@gmail.com>
2025-06-14 18:15:45 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDemoApiKey123456789",
+  authDomain: "flarglepox-demo.firebaseapp.com",
+  projectId: "flarglepox-demo",
+  storageBucket: "flarglepox-demo.appspot.com",
+  messagingSenderId: "123456789000",
+  appId: "1:123456789000:web:demoappid98765"
+};
