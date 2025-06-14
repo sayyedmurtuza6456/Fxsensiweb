35f6b094325c569b987437d2142c6068747c34e8
sayyedmurtuza6456 <sayyedaliop942113@gmail.com>
2025-06-14 18:30:27 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCk0abQ3jgMKkz_H0oX...",  // Use full key if needed
+  authDomain: "farglepox-firebase.firebaseapp.com",
+  projectId: "farglepox-firebase",
+  storageBucket: "farglepox-firebase.appspot.com",
+  messagingSenderId: "778584332662",
+  appId: "1:778584332662:web:XXXXXXXXXXXXXX"  // Use full appId if needed
+};
