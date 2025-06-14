340cce411d7a89d0333fd91a601f01875b0c739d
sayyedmurtuza6456 <sayyedaliop942113@gmail.com>
2025-06-14 18:29:12 +0530

    Delete firebase-config.js
    Sjjds

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDemoApiKey123456789",
-  authDomain: "flarglepox-demo.firebaseapp.com",
-  projectId: "flarglepox-demo",
-  storageBucket: "flarglepox-demo.appspot.com",
-  messagingSenderId: "123456789000",
-  appId: "1:123456789000:web:demoappid98765"
-};
