2ef9176950967afd9a951b52b500a786ab20c0a2
sayyedmurtuza6456 <sayyedaliop942113@gmail.com>
2025-06-14 18:13:54 +0530

    Delete firebase-config.js
    Ruru

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD-DUMMY-KEY-HERE-xYz123",
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-app",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
-};
